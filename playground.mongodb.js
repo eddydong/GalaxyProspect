f4875b63dbda16c19bd0207076f49f994f3644c1
eddy1199 <eddy1199@qq.com>
2025-08-16 15:03:22 +0800

    a

--- playground.mongodb.js
+++ playground.mongodb.js
@@ -1,8 +1,14 @@
 // MongoDB Playground
+
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 
 // The current database to use.
 use("MOWATCH");
 
+// db.daily_fact.updateMany(
+//     { "Value": { $exists: true } },
+//     [{ $set: { value: "$value" } }]
+// );
+
 // Find a document in a collection.
-db.daily_fact.find().sort({ _id: -1 }).limit(100).toArray();
+db.daily_fact.find().sort({ _id: -1 }).limit(200).toArray();
